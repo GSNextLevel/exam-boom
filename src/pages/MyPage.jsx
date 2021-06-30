f64f40223b9068eacfc21d2c158757b77b169e56
boomkim <bhk3177@gmail.com>
2021-06-30 18:08:27 +0900

    my page 작업중

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MyPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>email: </p>
+                <p>별명: </p>
+                <p>이메일 인증여부: </p>
+            </div>
+        );
+    }
+}
+
+export default MyPage;
